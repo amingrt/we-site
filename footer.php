1d0483c7f79bafbbb020162aff832c9e395127dd
amingrt <amin.gharaat12@gmail.com>
2021-12-01 13:50:06 +0330

    send for git

---
+++ footer.php
@@ -0,0 +1,76 @@
+
+<!--start-footer-->
+<div class="container-fluid position-absolute bg-footer p-0">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 col-md-3 coustom-footer">
+        <div class="Communication">
+          <p class="mt-5 p-color">ارتباط با ما</p>
+          <a href="#"><p>آدرس:استهبان- پیام نور-ساختمان مرکز رشد</p></a>
+          <a href="#"><p>ایمیل:chenarweb@gmail.com</p></a>
+          <a href="#"><p>تلفن:07153224545</p></a>
+          <a href="#"><p>تلفن همراه:09179174545</p></a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-3 coustom-footer">
+        <div class="quick-access">
+          <p class="mt-5 p-color">دسترسی سریع</p>
+          <a href="#"><p>طراحی سایت</p></a>
+          <a href="#"><p>مشاوره</p></a>
+          <a href="#"><p>وبلاگ</p></a>
+          <a href="#"><p>پشتیبانی</p></a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-3 coustom-footer">
+        <div class="quick-access">
+          <p class="mt-5 p-color">دسترسی سریع</p>
+          <a href="#"><p>آموزش ها</p></a>
+          <a href="#"><p>تعرفه طراحی سایت</p></a>
+          <a href="#"><p>قوانین</p></a>
+          <a href="#"><p>مقالات</p></a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-3 coustom-footer">
+
+ <div class="Social-Networks">
+  
+  <p class="mt-5 p-color"> شبکه های اجتماهی</p>
+  <div class="icon-footer mt-3">
+  <a href="#" class="instagram"><i class="fab fa-instagram fs-3"></i></a>
+<a href="#" >  <img class="telegram" src="img/telegram-plane-brands.svg" alt=""></a>
+  <a href="#" ><img class="twitter" src="img/twitter-brands.svg" alt=""></a>
+  <a href="#" ><img class="whatsapp" src="img/whatsapp-brands.svg" alt=""></a>
+</div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+<div class="end-footer">
+  <p>ساخته شده با عشق و علاقه توسط وب چنار</p>
+</div>
+
+
+
+<!--end-footer-->
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Chenar</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.rtl.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<!--start-top-nav-->
+<header>
+ <div class="top-header">
+     <div class="container">
+       <div class="row">
+           <div class="col-md-3 d-flex custom-top-nav">
+               <div class="icon">
+                 <div class="d-flex">
+                   <img src="img/web_logo_chenar2.png" alt="">
+                 </div>
+                 </div>
+                 <p class="title-img d-none d-lg-block">آموزش و طراحی سایت در وب چنار</p>
+              </div>
+               <div class="col-md-5 d-flex custom-top-nav d-flex align-items-center justify-content-center">
+                 <p class="Slogan d-none d-lg-block">بهتر دیده شدن را از ما بخواهید</p> </div>
+               <div class="col-md-4 d-flex custom-top-nav d-flex align-items-center justify-content-center">
+               <div class="sefaresh ">
+                 <button type="button" class=" mx-4 d-none d-lg-block"><span>سفارش طراحی سایت</span></button>
+                </div>
+                <div class="moshavereh ">
+                  <button type="button" class="d-none d-lg-block"><span> مشاوره رایگان</span></button>
+                </div>
+              </div>
+              <div class="icon-hide d-flex justify-content-end">
+        <div class="moshavere-icon mx-3 ">
+         <a href="#"> <img src="img/user-friends-solid.svg" alt="moshavere" class="d-none "></a>
+        </div>
+        <div class="sefaresh-icon">
+<a href="#"><img src="img/desktop-solid.svg" alt="sefaresh" class="d-none"></a>
+        </div>
+       </div>
+      </div>
+   </div>   
+  </div>      
+
+
+<!--end-top-nav-->
+<!--start-nav-->
+<div class="main-header">
+  
+  <div class="container">
+    <div class="row ">
+                <div class="col-sm-12 col-lg-10 navbar-light  custom-nav">
+                  <nav class="navbar navbar-expand-lg">
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">صفحه اصلی</a>
+                          </li>
+                          <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">طراحی سایت</a>
+                            <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="#">طراحی سایت وردپرس</a></li>
+                              <li><a class="dropdown-item" href="#">طراحی سایت شرکتی</a></li>
+                              <li><a class="dropdown-item" href="#">طراحی سایت شخصی</a></li>
+                              <li><a class="dropdown-item" href="#">طراحی سایت پزشکی</a></li>
+                              <li><a class="dropdown-item" href="#">طراحی سایت پزشکی</a></li>
+                            </ul>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link ">آموزش ها</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link ">وبلاگ</a>
+                          </li>
+                      
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">مقالات</a>
+                          <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">طراحی سایت </a></li>
+                            <li><a class="dropdown-item" href="#">برنامه نویسی</a></li>
+                            <li><a class="dropdown-item" href="#">هاست</a></li>
+                          </ul>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">درباره ما</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link " href="#">ارتباط با ما</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link " href="#">مشاوره</a>
+                        </li>
+
+
+
+
+                        
+                        <li class="nav-item">
+                         <!-- Button trigger modal -->
+                         <button type="button" class="btn-login" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                           <img src="img/sign-in-alt-solid.svg" alt="">
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><span>ثبت نام / ورود به سایت</span></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3">
+          <label for="formGroupExampleInput" class="form-label">ایمیل </label>
+          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="ایمیل خود را وارد کنید">
+        </div>
+        <div class="mb-3">
+          <label for="formGroupExampleInput2" class="form-label">رمز عبور</label>
+          <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="رمز عبور خود را وارد کنید">
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" >
+          <label class="form-check-label" for="flexCheckChecked">
+            مرا به خاطر بسپار
+          </label>
+        </div>
+        <button type="button" class="btn btn-login w-100 ">ورود</button>
+        <button type="button" class="btn btn-login-google w-100 mb-3">ورود با گوگل</button>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked1" >
+          <label class="form-check-label mb-3" for="flexCheckChecked1">
+            من ربات نیستم
+          </label>
+        </div>
+        <div class="modal-footer justify-content-center">
+          <a href="#"><p>ثبت نام نکرده اید؟ کلیک کنید</p></a>
+          <a href="#"><p>رمز عبور خود را فراموش کرده اید؟</p></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</li>
+  
+
+
+                          <li class="nav-item px-3 ">
+
+                            <a class="btn" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                              <img src="img/search-solid (1).svg" style="height: 15px;width: 15px;" alt="">
+                            </a>
+                       
+                        
+                          <div class="collapse" id="collapseExample">
+                            <div class="card card-body">
+                            <form action="https://google.com">
+                              <input type="search" class="form-control" id="collapseExample" placeholder="عنوان مد نظر خود را جست و جو کنید">
+                            </form>
+                            </div>
+                          </div>
+                  
+                          </li>
+
+                        </ul>
+
+                      </div>
+                    </nav>
+                    </div>
+                  </div>
+  <!--end-nav-->
+  </div>
+</div>
+</header>
