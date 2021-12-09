bd075c15ac3854b07ab2c6d78964917145c03dbe
amingrt <amin.gharaat12@gmail.com>
2021-12-09 18:03:25 +0330

    index

---
+++ index.php
@@ -0,0 +1,490 @@
+<?php include_once('./header.php');?>
+
+
+
+
+<!--start slider-->
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12 coustom-slider">
+      <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active position-relative" >
+            <img src="img/best-programing-languages.jpg " class="d-block w-100 img-fluid " alt="...">
+            <div class="crasol-caption position-absolute">
+<h6>مرکز تخصصی طراحی سایت</h6>
+<p>صفر تا صد طراحی سایت تخصص ماست</p>
+            </div>
+          </div>
+          <div class="carousel-item ">
+            <img src="img/39798670-creative-mind.jpg" class="d-block w-100" alt="...">
+            <div class="crasol-caption-two position-absolute">
+              <h6>شعار ما </h6>
+              <p>خلاقیت اتفاقی نیست</p>
+                          </div>
+          </div>
+
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--end slider-->
+
+<!--start services-->
+<div class="container-fluid bg-light">
+<div class="container py-2 ">
+  <div class="row py-5 ">
+    <div class="services-title">
+      <h5 class="text-center p-5 coustom-start-services">
+        <span class="py-5 position-relative">برخی از خدمات چنار وب</span>
+      </h5>
+
+    </div>
+    <div class="col-xs-12 col-sm-6 col-lg-4 coustom-services mb-3">
+      <a href="#">
+      <div class="card">
+          <img src="img/4hnaohsgnavl.jpeg " class="card-img-top img-fluid" alt="...">
+          <div class="card-body">
+<h5 class="card-title text-center">طراحی سایت های فروشگاهی</h5>
+  </div>
+</div>
+</a>
+ 
+
+
+
+  </div>
+
+  <div class="col-xs-12 col-sm-6 col-lg-4 coustom-services mb-3">
+    <a href="#">
+    <div class="card">
+      <img src="img/5f1ucpap46r7.jpeg" class="card-img-top img-fluid" alt="...">
+      <div class="card-body">
+<h5 class="card-title text-center">طراحی سایت با php و laravel</h5>
+</div>
+</div>
+</a>
+    
+
+
+
+
+</div>
+
+<div class="col-xs-12 col-sm-6 col-lg-4 coustom-services mb-3">
+  <a href="#">
+  <div class="card">
+    <img src="img/what-is-wordpress.png" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+ <h5 class="card-title text-center">طراحی سایت های wordpress</h5>
+</div>
+</div>
+</a>
+  
+
+
+
+
+</div>
+
+<div class="col-xs-12 col-sm-6 col-lg-4 coustom-services mb-3">
+  <a href="#">
+  <div class="card">
+    <img src="img/security.jpg" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+<h5 class="card-title text-center">امنیت وبسایت</h5>
+</div>
+</div>
+</a>
+  
+
+
+
+
+</div>
+
+<div class="col-xs-12 col-sm-6 col-lg-4 coustom-services mb-3">
+  <a href="#">
+  <div class="card">
+    <img src="img/home-1-blog-img-1.jpg" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+<h5 class="card-title text-center">طراحی سایت های اختصاصی</h5>
+</div>
+</div>
+</a>
+
+
+
+
+</div>
+
+<div class="col-xs-12 col-sm-6 col-lg-4 coustom-services mb-3">
+  <a href="#">
+  <div class="card">
+    <img src="img/seo-min.png" class="card-img-top img-fluid" alt="...">
+    <div class="card-body">
+<h5 class="card-title text-center mb-2">بهینه سازی سایت</h5>
+</div>
+</div>
+</a>
+
+
+
+
+</div>
+</div>
+<a href="#"><p class="d-flex justify-content-center footer-service">مشاهده همه خدمات ما</p></a>
+</div>
+</div>
+<!--end services-->
+<!--start-Design-process-->
+<div class="container-fluid bg-white">
+  <div class="container">
+  <div class="row">
+    <div class="design-process-title">
+      <h5 class="coustom-h5-Design-process p-5 text-center">
+        <span class="py-5 position-relative">روند طراحی سایت در شرکت ما</span>
+      </h5>
+      </div>
+    <div class="col-sm-12 col-lg-6 coustom-design-process py-3">
+      <div class="">
+        <img src="img/icons8-done-20.png" class="" alt=""><p>مشاوره </p>
+        <img src="img/icons8-done-20.png" class="" alt=""><p>برسی نیازها </p>
+        <img src="img/icons8-done-20.png" class="" alt=""><p>طراحی اولیه وب سایت </p>
+        <img src="img/icons8-done-20.png" class="" alt=""><p>درصورت تایید مشتری،شروع پروژه </p>
+        <img src="img/icons8-done-20.png" class="" alt=""><p>تاییئ نهایی،تست برنامه و برطرف سازی مشکلات </p>
+        <img src="img/icons8-done-20.png" class="" alt=""><p>تحویل پروژه به مشتری </p>
+      </div>
+      </div>
+
+
+
+
+
+    <div class="col-sm-12 d-none d-md-block col-md-6">
+<div class="img-design-process ">
+  <img src="img/Work chat-bro (1).png" class="" alt="">
+</div>
+    </div>
+  </div>
+</div>
+</div>
+<!--end-Design-process-->
+
+<!--start-Property-Web-design-->
+
+
+<div class="container-fluid bg-light">
+  <div class="container">
+    <div class="row">
+      <div class="Property-Web-design-title">
+        <h5 class="coustom-h5-Web-design text-center p-5">
+          <span class="py-5 position-relative">ویژگی های طراحی وب در وب چنار</span>
+        </h5>
+        </div>
+        <div class="col-sm-12 d-none d-md-block col-md-6 d-flex">
+          <div class="img-Property-Web-design">
+            <img src="img/Search engines-bro.png" alt="">
+          </div>
+              </div>
+      <div class="col-sm-12 col-lg-6 coustom-Property-Web-design py-3 d-flex">
+        <div class="contant">
+          
+          <img src="img/icons8-done-20.png" class="" alt=""><p>استفاده از بروزترین تکنولوژی ها </p>
+          <img src="img/icons8-done-20.png" class="" alt=""><p>نازل ترین قیمت </p>
+          <img src="img/icons8-done-20.png" class="" alt=""><p>تحویل به موقع  </p>
+          <img src="img/icons8-done-20.png" class="" alt=""><p>پشتیبانی 3 ماهه رایگان </p>
+          <img src="img/icons8-done-20.png" class="" alt=""><p>امنیت بالای وب سایت </p>
+        </div>
+        </div>
+   
+  
+  
+  
+  
+
+    </div>
+  </div>
+  </div>
+<!--end-Property-Web-design-->
+
+
+<!--start-final-article-->
+
+
+<div class="container-fluid bg-white">
+  <div class="container">
+    <div class="row">
+      <div>
+        <h5 class=" coustom-h5-article text-center p-5">
+          <span class="py-5 position-relative">آخرین مقالات</span>
+        </h5>
+        </div>
+      <div class="col-xs-12 col-sm-6 col-lg-4 coustom-final-article mb-3">
+        <div class="card d-flex ">
+    <a href="#">
+    <img src="img/Conclusion.png" class="card-img-top img-fluid border-bottom" alt="...">
+  </a>
+    <div class="card-body position-relative">
+      <div class="tarikh position-absolute">
+        <div class="roze text-center">
+          <span class="">23</span>
+          
+        </div>
+        <div class="mah text-center">
+          <span>شهریور</span>
+        </div>
+      </div>
+      
+      <a href="#" class="card-title px-3 "><p class="text-center">سوالات متدوال شروع برنامه نویسی</p></a>
+      <p class="card-text px-3 ">وقتی شروع به یادگیری یک زبان برنامه نویسی کنید، در طول یادگیری زبان با الگوریتم ها هم آشنا خواهیدخواهید شد.
+        نیازی نیست که حتماً در ابتدا به طور کامل با الگوریتم ها...
+        <a href="#" class="read-more">ادامه مطلب</a></p>
+      </div>
+      <div class="icon">
+        <ul>
+          <li><img src="img/icons8-edit-24.png" alt=""><span>برنامه نویسی</span></li>
+          <li><img src="img/heart-thin.png" alt="" ><span>120</span></li>
+          <li><img src="img/icons8-eye-24.png" alt=""><span>65</span></li>
+        </ul>
+        
+        
+        
+      </div>
+    </div>
+
+  </div>
+  <div class="col-xs-12 col-sm-6 col-lg-4 coustom-final-article mb-3">
+    <div class="card d-flex">
+      <a href="#">
+              <img src="img/security-laravel.png" class="card-img-top border-bottom" alt="...">
+            </a>
+              <div class="card-body position-relative">
+                <div class="tarikh position-absolute">
+                <div class="roze text-center">
+                  <span class="">25</span>
+    
+                </div>
+                <div class="mah text-center">
+                  <span>مهر</span>
+                </div>
+              </div>
+          
+                <a href="#" class="text-center card-title px-3 "><p class="text-center">حفاظت CSRF در لاراول چیست؟</p></a>
+                <p class="card-text px-3">فریمورک محبوب لاراول، در عین سادگی دارای نکات و جزئیات فراوانی است 
+                  که یادگیری آن‌ها نیازمند دقت و صرف زمان مناسب می‌باشد.
+                   یکی از نکات مهم در بحث امنیت ....
+
+                <a href="#" class="read-more">ادامه مطلب</a></p>
+              </div>
+                    <div class="icon">
+    <ul>
+      <li><img src="img/icons8-edit-24.png" alt=""><span>لاراول</span></li>
+      <li><img src="img/heart-thin.png" alt="" ><span>120</span></li>
+      <li><img src="img/icons8-eye-24.png" alt=""><span>65</span></li>
+    </ul>
+    
+    
+  </div>
+</div>
+
+              </div>
+              <div class="col-xs-12 col-sm-6 col-lg-4 coustom-final-article mb-3">
+                <div class="card d-flex">
+                  <a href="#">
+                  <img src="img/snackbar.png" class="card-img-top img-fluid border-bottom" alt="...">
+                </a>
+                  <div class="card-body position-relative ">
+                    <div class="tarikh position-absolute">
+                    <div class="roze text-center">
+                      <span class="">29</span>
+        
+                    </div>
+                    <div class="mah text-center">
+                      <span>شهریور</span>
+                    </div>
+                  </div>
+              
+                    <a href="#" class="card-title px-3 "><p class="text-center">آموزش استفاده از snack bar در اندروید</p></a>
+                    <p class="card-text px-3">آموزش استفاده از Snack Bar در تفاده از Snack Bar بسیار راحت بوده و در عین حال تجربه کاربری خیلی خوبی  می‌نین این ابی برای Toast در اپلیکیشن‌های اندرویدی شما باشد...
+                    <a href="#" class="read-more">ادامه مطلب</a></p>
+                  </div>
+                        <div class="icon">
+        <ul>
+          <li><img src="img/icons8-edit-24.png" alt=""><span>اندروید</span></li>
+          <li><img src="img/heart-thin.png" alt="" ><span>320</span></li>
+          <li><img src="img/icons8-eye-24.png" alt=""><span>5</span></li>
+        </ul>
+        
+        
+        
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col-xs-12 col-sm-6 col-lg-4 coustom-final-article mb-3">
+                    <div class="card d-flex">
+                      <a href="#">
+                      <img src="img/what php.png" class="card-img-top img-fluid border-bottom" alt="...">
+                    </a>
+                      <div class="card-body position-relative">
+                        <div class="tarikh position-absolute">
+                        <div class="roze text-center">
+                          <span class="">23</span>
+            
+                        </div>
+                        <div class="mah text-center">
+                          <span>آبان</span>
+                        </div>
+                      </div>
+                  
+                        <a href="#" class="card-title px-3 "><p class="text-center">چرا برنامه نویس php شوم؟
+                        </p></a>
+                        <p class="card-text px-3">زبان برنامه نویسی PHP یکی از زبان های برنامه نویسی سمت سرور است که کاربر، در پیادیکی (پویا) است. بازنویسی هسته PHP و ایریخچه زبان برنامه نویسی PHP ایجاد کرد...
+                        <a href="#" class="read-more">ادامه مطلب</a></p>
+                      </div>
+                            <div class="icon">
+            <ul>
+              <li><img src="img/icons8-edit-24.png" alt=""><span>php</span></li>
+              <li><img src="img/heart-thin.png" alt="" ><span>420</span></li>
+              <li><img src="img/icons8-eye-24.png" alt=""><span>605</span></li>
+            </ul>
+            
+            
+            
+                            </div>
+                          </div>
+                      </div>
+                      <div class="col-xs-12 col-sm-6 col-lg-4 coustom-final-article mb-3">
+                        <div class="card d-flex">
+                          <a href="#">
+                          <img src="img/js.jpg" class="card-img-top img-fluid border-bottom" alt="...">
+                        </a>
+                          <div class="card-body position-relative">
+                            <div class="tarikh position-absolute">
+                            <div class="roze text-center">
+                              <span class="">29</span>
+                
+                            </div>
+                            <div class="mah text-center">
+                              <span>آبان</span>
+                            </div>
+                          </div>
+                      
+                            <a href="#" class="card-title px-3 "><p class="text-center">چرا برنامه نویس js شویم؟</p></a>
+                            <p class="card-text px-3">زبان برنامه نویسی js یکی از زبان های برنامه نویسی سمت سرور است که کاربر، در پیادیکی (پویا) است. بازنویسی هسته PHP و ایریخچه زبان برنامه نویسی PHP ایجاد کرد...
+                            <a href="#" class="read-more">ادامه مطلب</a></p>
+                          </div>
+                                <div class="icon">
+                <ul>
+                  <li><img src="img/icons8-edit-24.png" alt=""><span>جاوا اسکریپت</span></li>
+                  <li><img src="img/heart-thin.png" alt="" ><span>282</span></li>
+                  <li><img src="img/icons8-eye-24.png" alt=""><span>92</span></li>
+                </ul>
+                
+                
+                
+                                </div>
+                              </div>
+                          </div>
+                          <div class="col-xs-12 col-sm-6 col-lg-4 coustom-final-article mb-3">
+                            <div class="card d-flex">
+                              <a href="#">
+                              <img src="img/flutter2.png" class="card-img-top img-fluid border-bottom" alt="...">
+                            </a>
+                              <div class="card-body position-relative">
+                                <div class="tarikh position-absolute">
+                                <div class="roze text-center">
+                                  <span class="">23</span>
+                    
+                                </div>
+                                <div class="mah text-center">
+                                  <span>آذر</span>
+                                </div>
+                              </div>
+                          
+                                <a href="#" class="card-title px-3 "><p class="text-center">چرا برنامه نویس فلاتر شویم؟</p></a>
+                                <p class="card-text px-3">زبان برنامه نویسی PHP یکی از زبان های برنامه نویسی سمت سرور است که کاربر، در پیادیکی (پویا) است. بازنویسی هسته PHP و ایریخچه زبان برنامه نویسی PHP ایجاد کرد...
+                                <a href="#" class="read-more">ادامه مطلب</a></p>
+                              </div>
+                                    <div class="icon">
+                    <ul>
+                      <li><img src="img/icons8-edit-24.png" alt=""><span>اندروید</span></li>
+                      <li><img src="img/heart-thin.png" alt="" ><span>10</span></li>
+                      <li><img src="img/icons8-eye-24.png" alt=""><span>650</span></li>
+                    </ul>
+                    
+                    
+                    
+                                    </div>
+                                  </div>
+                              </div>
+                              
+        </div>
+        
+        <a href="#"><p class="d-flex justify-content-center footer-service mt-5 ">مشاهده همه مقالات</p></a>
+        
+        
+   
+
+
+      </div>
+    </div> 
+
+
+
+<!--end-final-article-->
+
+
+
+<!--start-Newsletters-->
+
+<div class="container-fluid ">
+  <div class="container coustom-newsletters">
+  <div class="row">
+    <div class="col-sm-12">
+<div class="Newsletters">
+  <p class="text-center py-2">عضویت در خبرنامه</p>
+  <p class="text-center p-define pb-2">با عضویت در خبرنامه ایمیلی از 20 % تخفیف و همچنین مشاوره رایگان بهرمند شوید</p>
+  <div class="col-sm-12 d-flex justify-content-center">
+  <div class="input-group mb-3 w-50 pb-5 ">
+  <div class="form d-flex mt-2">  
+    <input type="text" class="form-control" placeholder="ایمیل خود را وارد کنید" aria-label="Recipient's username" aria-describedby="basic-addon2">
+    <button class="">ثبت نام</button></div>
+  </div>
+</div>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+    
+
+   
+
+
+
+
+
+
+
+<!--end-Newsletters-->
+
+
+
+
+
+<?php include_once('./footer.php');?>
